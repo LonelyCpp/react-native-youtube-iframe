93c015c95144472e46a3c46471b8922e099a7e1a
Ananthu Kanive <ananthu.kanive@smallcase.com>
2019-12-15 13:38:42 +0530

    move web view scripts to its own file

---
+++ src/PlayerScripts.js
@@ -0,0 +1,140 @@
+export const PLAYER_FUNCTIONS = {
+  durationScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getDuration', data: player.getDuration()}));
+true;
+`,
+  currentTimeScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getCurrentTime', data: player.getCurrentTime()}));
+true;
+`,
+  isMutedScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'isMuted', data: player.isMuted()}));
+true;
+`,
+  getVolumeScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getVolume', data: player.getVolume()}));
+true;
+`,
+  getPlaybackRateScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getPlaybackRate', data: player.getPlaybackRate()}));
+true;
+`,
+  getAvailablePlaybackRatesScript: `
+window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getAvailablePlaybackRates', data: player.getAvailablePlaybackRates()}));
+true;
+`,
+  seekToScript: (seconds, allowSeekAhead) => `
+player.seekTo(${seconds}, ${allowSeekAhead})
+`,
+  playVideo: 'player.playVideo(); true;',
+  pauseVideo: 'player.pauseVideo(); true;',
+  muteVideo: 'player.mute(); true;',
+  unMuteVideo: 'player.unMute(); true;',
+  setPlaybackRate: playbackRate =>
+    `player.setPlaybackRate(${playbackRate}); true;`,
+  setVolume: volume => `player.setVolume(${volume}); true;`,
+};
+
+export const MAIN_SCRIPT = (
+  videoId,
+  {
+    loop = false,
+    controls = true,
+    cc_lang_pref, // country code
+    showClosedCaptions,
+    color, // 'red' or 'white'
+    end,
+    preventFullScreen = false,
+    playerLang,
+    iv_load_policy,
+    modestbranding,
+    rel,
+    start,
+  },
+) => `<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <style>
+      .container {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 56.25%;
+      }
+      .video {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="video" id="player" />
+    </div>
+
+    <script>
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '1000',
+          width: '1000',
+          videoId: '${videoId}',
+          playerVars: {
+            playsinline: 1,
+            loop: ${loop ? 1 : 0},
+            controls: ${controls ? 1 : 0},
+            cc_lang_pref: '${cc_lang_pref || ''}',
+            cc_load_policy: ${showClosedCaptions ? 1 : 0},
+            color: ${color},
+            end: ${end},
+            fs: ${preventFullScreen ? 0 : 1},
+            hl: ${playerLang},
+            iv_load_policy: ${iv_load_policy},
+            modestbranding: ${modestbranding ? 1 : 0},
+            rel: ${rel ? 1 : 0},
+            start: ${start},
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange,
+            'onError': onPlayerError,
+            'onPlaybackQualityChange': onPlaybackQualityChange,
+            'onPlaybackRateChange': onPlaybackRateChange,
+          }
+        });
+      }
+
+      function onPlayerError(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerError', data: event.data}))
+      }
+
+      function onPlaybackRateChange(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playbackRateChange', data: event.data}))
+      }
+
+      function onPlaybackQualityChange(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerQualityChange', data: event.data}))
+      }
+
+      function onPlayerReady(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerReady'}))
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerStateChange', data: event.data}))
+      }
+    </script>
+  </body>
+</html>`;
--- src/YoutubeIframe.js
+++ src/YoutubeIframe.js
@@ -4,12 +4,13 @@ import React, {
   useCallback,
   useImperativeHandle,
   forwardRef,
-  useState
-} from "react";
-import { View } from "react-native";
-import WebView from "react-native-webview";
-import { PLAYER_STATES, PLAYER_ERROR } from "./constants";
-import { EventEmitter } from "events";
+  useState,
+} from 'react';
+import {View} from 'react-native';
+import WebView from 'react-native-webview';
+import {PLAYER_STATES, PLAYER_ERROR} from './constants';
+import {EventEmitter} from 'events';
+import {MAIN_SCRIPT, PLAYER_FUNCTIONS} from './PlayerScripts';
 
 const YoutubeIframe = (
   {
@@ -25,9 +26,9 @@ const YoutubeIframe = (
     volume = 100,
     playbackRate = 1,
     onPlaybackRateChange,
-    initialPlayerParams = {}
+    initialPlayerParams = {},
   },
-  ref
+  ref,
 ) => {
   const webViewRef = useRef(null);
   const eventEmitter = useRef(new EventEmitter());
@@ -37,69 +38,73 @@ const YoutubeIframe = (
     ref,
     () => ({
       getDuration: () => {
-        webViewRef.current.injectJavaScript(durationScript);
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.durationScript);
         return new Promise(resolve => {
-          eventEmitter.current.once("getDuration", resolve);
+          eventEmitter.current.once('getDuration', resolve);
         });
       },
       getCurrentTime: () => {
-        webViewRef.current.injectJavaScript(currentTimeScript);
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.currentTimeScript);
         return new Promise(resolve => {
-          eventEmitter.current.once("getCurrentTime", resolve);
+          eventEmitter.current.once('getCurrentTime', resolve);
         });
       },
       isMuted: () => {
-        webViewRef.current.injectJavaScript(isMutedScript);
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.isMutedScript);
         return new Promise(resolve => {
-          eventEmitter.current.once("isMuted", resolve);
+          eventEmitter.current.once('isMuted', resolve);
         });
       },
       getVolume: () => {
-        webViewRef.current.injectJavaScript(getVolumeScript);
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.getVolumeScript);
         return new Promise(resolve => {
-          eventEmitter.current.once("getVolume", resolve);
+          eventEmitter.current.once('getVolume', resolve);
         });
       },
       getPlaybackRate: () => {
-        webViewRef.current.injectJavaScript(getPlaybackRateScript);
+        webViewRef.current.injectJavaScript(
+          PLAYER_FUNCTIONS.getPlaybackRateScript,
+        );
         return new Promise(resolve => {
-          eventEmitter.current.once("getPlaybackRate", resolve);
+          eventEmitter.current.once('getPlaybackRate', resolve);
         });
       },
       getAvailablePlaybackRates: () => {
-        webViewRef.current.injectJavaScript(getAvailablePlaybackRatesScript);
+        webViewRef.current.injectJavaScript(
+          PLAYER_FUNCTIONS.getAvailablePlaybackRatesScript,
+        );
         return new Promise(resolve => {
-          eventEmitter.current.once("getAvailablePlaybackRates", resolve);
+          eventEmitter.current.once('getAvailablePlaybackRates', resolve);
         });
       },
       seekTo: (seconds, allowSeekAhead) => {
         webViewRef.current.injectJavaScript(
-          seekToScript(seconds, allowSeekAhead)
+          PLAYER_FUNCTIONS.seekToScript(seconds, allowSeekAhead),
         );
         return new Promise(resolve => {
-          eventEmitter.current.once("getAvailablePlaybackRates", resolve);
+          eventEmitter.current.once('getAvailablePlaybackRates', resolve);
         });
-      }
+      },
     }),
-    []
+    [],
   );
 
   useEffect(() => {
     if (playerReady) {
       if (play) {
-        webViewRef.current.injectJavaScript("player.playVideo(); true;");
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.playVideo);
       } else {
-        webViewRef.current.injectJavaScript("player.pauseVideo(); true;");
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.pauseVideo);
       }
 
       if (mute) {
-        webViewRef.current.injectJavaScript("player.mute(); true;");
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.muteVideo);
       } else {
-        webViewRef.current.injectJavaScript("player.unMute(); true;");
+        webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.unMuteVideo);
       }
-      webViewRef.current.injectJavaScript(`player.setVolume(${volume}); true;`);
+      webViewRef.current.injectJavaScript(PLAYER_FUNCTIONS.setVolume(volume));
       webViewRef.current.injectJavaScript(
-        `player.setPlaybackRate(${playbackRate}); true;`
+        PLAYER_FUNCTIONS.setPlaybackRate(playbackRate),
       );
     }
   }, [play, playerReady, mute, volume, playbackRate]);
@@ -110,20 +115,20 @@ const YoutubeIframe = (
       // console.log({message});
       try {
         switch (message.eventType) {
-          case "playerStateChange":
+          case 'playerStateChange':
             onChangeState(PLAYER_STATES[message.data]);
             break;
-          case "playerReady":
+          case 'playerReady':
             onReady();
             setPlayerReady(true);
             break;
-          case "playerQualityChange":
+          case 'playerQualityChange':
             onPlaybackQualityChange(message.data);
             break;
-          case "playerError":
+          case 'playerError':
             onError(PLAYER_ERROR[message.data]);
             break;
-          case "playbackRateChange":
+          case 'playbackRateChange':
             onPlaybackRateChange(message.data);
             break;
           default:
@@ -139,160 +144,21 @@ const YoutubeIframe = (
       onReady,
       onPlaybackQualityChange,
       onError,
-      onPlaybackRateChange
-    ]
+      onPlaybackRateChange,
+    ],
   );
 
   return (
-    <View style={{ height, width }}>
+    <View style={{height, width}}>
       <WebView
         ref={webViewRef}
-        originWhitelist={["*"]}
-        source={{ html: script(videoId, initialPlayerParams) }}
+        originWhitelist={['*']}
+        source={{html: MAIN_SCRIPT(videoId, initialPlayerParams)}}
         allowsInlineMediaPlayback
         onMessage={onWebMessage}
       />
     </View>
   );
 };
-
-const durationScript = `
-window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getDuration', data: player.getDuration()}));
-true;
-`;
-
-const currentTimeScript = `
-window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getCurrentTime', data: player.getCurrentTime()}));
-true;
-`;
-
-const isMutedScript = `
-window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'isMuted', data: player.isMuted()}));
-true;
-`;
-
-const getVolumeScript = `
-window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getVolume', data: player.getVolume()}));
-true;
-`;
-
-const getPlaybackRateScript = `
-window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getPlaybackRate', data: player.getPlaybackRate()}));
-true;
-`;
-
-const getAvailablePlaybackRatesScript = `
-window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'getAvailablePlaybackRates', data: player.getAvailablePlaybackRates()}));
-true;
-`;
-
-const seekToScript = (seconds, allowSeekAhead) => `
-player.seekTo(${seconds}, ${allowSeekAhead})
-`;
-
-const script = (
-  videoId,
-  {
-    loop = false,
-    controls = true,
-    cc_lang_pref, // country code
-    showClosedCaptions,
-    color, // 'red' or 'white'
-    end,
-    preventFullScreen = false,
-    playerLang,
-    iv_load_policy,
-    modestbranding,
-    origin,
-    rel,
-    start
-  }
-) => `<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-    <style>
-      .container {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding-bottom: 56.25%;
-      }
-      .video {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <div class="video" id="player" />
-    </div>
-
-    <script>
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '1000',
-          width: '1000',
-          videoId: '${videoId}',
-          playerVars: {
-            playsinline: 1,
-            loop: ${loop ? 1 : 0},
-            controls: ${controls ? 1 : 0},
-            cc_lang_pref: '${cc_lang_pref || ""}',
-            cc_load_policy: ${showClosedCaptions ? 1 : 0},
-            color: ${color},
-            end: ${end},
-            fs: ${preventFullScreen ? 0 : 1},
-            hl: ${playerLang},
-            iv_load_policy: ${iv_load_policy},
-            modestbranding: ${modestbranding ? 1 : 0},
-            rel: ${rel ? 1 : 0},
-            start: ${start},
-          },
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-            'onError': onPlayerError,
-            'onPlaybackQualityChange': onPlaybackQualityChange,
-            'onPlaybackRateChange': onPlaybackRateChange,
-          }
-        });
-      }
-
-      function onPlayerError(event) {
-        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerError', data: event.data}))
-      }
-
-      function onPlaybackRateChange(event) {
-        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playbackRateChange', data: event.data}))
-      }
-
-      function onPlaybackQualityChange(event) {
-        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerQualityChange', data: event.data}))
-      }
-
-      function onPlayerReady(event) {
-        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerReady'}))
-      }
-
-      var done = false;
-      function onPlayerStateChange(event) {
-        window.ReactNativeWebView.postMessage(JSON.stringify({eventType: 'playerStateChange', data: event.data}))
-      }
-    </script>
-  </body>
-</html>`;
 
 export default forwardRef(YoutubeIframe);
