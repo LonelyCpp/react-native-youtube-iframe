8a171387ce677e43072e8b1f0389999a641281ab
Michał Kardyś <michal.kardys@synerise.com>
2020-10-03 14:55:23 +0200

    feat: export player error names

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import YoutubeIframe from './src/YoutubeIframe';
 import {getYoutubeMeta} from './src/oEmbed';
-import {PLAYER_STATES_NAMES as PLAYER_STATES} from './src/constants';
+import {
+  PLAYER_STATES_NAMES as PLAYER_STATES,
+  PLAYER_ERROR_NAMES as PLAYER_ERRORS,
+} from './src/constants';
 
 export default YoutubeIframe;
-export {getYoutubeMeta, PLAYER_STATES};
+export {getYoutubeMeta, PLAYER_STATES, PLAYER_ERRORS};
