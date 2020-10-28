96cd6eda0ead5abadb21e2a09d618961d2d9d3cb
성혁 <cjstjdgur1234@gmail.com>
2020-10-28 17:50:34 +0900

    fix: Check whether if 'webViewProps' is null
    `bounces={webViewProps.bounces ?? false}` has problem when webViewProps = null.
    Thus, change operation from '.' to '?.'

--- src/YoutubeIframe.js
+++ src/YoutubeIframe.js
@@ -191,7 +191,7 @@ const YoutubeIframe = (props, ref) => {
           return request.mainDocumentURL === 'about:blank';
         }}
         {...webViewProps}
-        bounces={webViewProps.bounces ?? false}
+        bounces={webViewProps?.bounces ?? false}
       />
     </View>
   );
